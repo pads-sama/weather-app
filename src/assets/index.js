ea9189e32ae49665dc087b6023c0e912d2d4a1a3
Juan Carlos Padillo <juancarlospadillo23@gmail.com>
2023-12-30 23:38:11 +0800

    update ui and added api

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,11 @@
 import Mountain from './morning-bg.webp'
 import Sunny from './sunny.png'
+import Search from './search.png'
+import SearchBlack from './search-black.png'
 
 export {
     Mountain,
-    Sunny
+    Sunny,
+    Search,
+    SearchBlack
 }
